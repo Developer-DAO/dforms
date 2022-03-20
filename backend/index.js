f33c994a6f52f909a16cdf723b548783a63d48ee
qedk <1994constant@gmail.com>
2022-03-20 21:29:08 +0530

    fix: Add missing modules

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const fastify = require('fastify')({ logger: true })
+const fastifySession = require('@fastify/session')
+const fastifyCookie = require('fastify-cookie')
 
+fastify.register(fastifyCookie)
+fastify.register(fastifySession, { secret: process.env.SECRET })
 fastify.register(require('./routes'))
 
 const start = async () => {
