f19a57f887805869fff35cc6b8cf4e13123bfa54
qedk <1994constant@gmail.com>
2022-03-20 20:39:21 +0530

    feat: Init backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
